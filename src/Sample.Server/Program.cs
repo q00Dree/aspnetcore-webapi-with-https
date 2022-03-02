a8017ca05664e7596c2bfa1f4d68d78828c75518
Artur Safiullin <shard.f@mail.ru>
2022-03-02 20:39:33 +0300

    [master] applied some features with port and added logging.

--- src/Sample.Server/Program.cs
+++ src/Sample.Server/Program.cs
@@ -1,20 +1,61 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using System;
+using System.IO;
 
 namespace Sample.Server
 {
     public class Program
     {
+        private const string _appsettingsFilename = "appsettings.json";
+        private const string _nlogConfigFilename = "NLog.config";
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = LogManager
+                .Setup()
+                .GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("Creating host and run");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception, "Stopped application because of an exception.");
+                throw;
+            }
+            finally
+            {
+                logger.Info("Application is shutting down...");
+                LogManager.Shutdown();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath: Directory.GetCurrentDirectory())
+                .AddJsonFile(path: _appsettingsFilename, optional: true, reloadOnChange: true)
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                    webBuilder.UseUrls(urls: "http://0.0.0.0:5000");
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddNLog(configFileName: _nlogConfigFilename);
+                })
+                .UseNLog();
+        }
     }
 }
